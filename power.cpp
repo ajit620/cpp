623f79f34e26688e1457dd1b2835a42c810a4a72
ajt620 <ajitpanja100@gmail.com>
2024-10-27 21:15:03 +0530

    ADDed

---
+++ power.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+int main(){
+    int power;
+    float a=2.5;
+    // = +0
+    // 5-9 =+1
+
+    // cout<<"enter a number";
+    // cin>>a;
+    power=round(2.7);
+    cout<<power<<endl;
+    return 0;
+} 
+// 0-4
